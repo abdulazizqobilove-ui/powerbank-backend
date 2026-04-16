2ece9f73fe80ece6183b79f15b5c55b679d436b3
aqobi <aqobi@gmail.com>
2026-04-16 23:27:31 +0500

    check db

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+print("DATABASE:", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
