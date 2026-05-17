e6ae1993c35a75b362dfbb2fe241fedd0980ecd0
aqobi <aqobi@gmail.com>
2026-05-17 09:47:42 +0500

    fix cload code

--- main.py
+++ main.py
@@ -39,8 +39,16 @@ mqtt_log = logging.getLogger("mqtt")
 # =========================
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
 if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+if "sslmode=" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
 
 MQTT_HOST     = os.getenv("MQTT_HOST", "localhost")
 MQTT_PORT     = int(os.getenv("MQTT_PORT", 1883))
